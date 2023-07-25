ba20dd40763770ba2d82e8e4469460b06d630cf9
unknown <knameless27@gmail.com>
2023-07-25 14:52:33 -0500

    feat: navbar

---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="menu">
+        <ul>
+            <li><a>Home</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.menu {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    overflow: hidden;
+    background-color: #333;
+}
+
+ul {
+    list-style-type: none;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: rgb(233, 229, 229);
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    cursor: pointer;
+    background-color: #111;
+    border-radius: 6px 6px 0 0;
+}
+</style>
